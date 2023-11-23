7049561734b72ac963d0a96990c21775302ba14d
Jonas Eschmann <jonas.eschmann@gmail.com>
2023-11-22 23:44:30 -0500

    cleaning up

--- src/ablation_study.cpp
+++ src/ablation_study.cpp
@@ -52,7 +52,7 @@ struct AblationSpecBase: parameters::DefaultAblationSpec{
     static constexpr TI NUM_RUNS = 50;
 };
 
-template <typename TI, bool T_DISTURBANCE, bool T_OBSERVATION_NOISE, bool T_ASYMMETRIC_ACTOR_CRITIC, bool T_ROTOR_DELAY, bool T_ACTION_HISTORY, bool T_ENABLE_CURRICULUM, bool T_USE_INITIAL_REWARD_FUNCTION, bool T_RECALCULATE_REWARDS, bool T_EXPLORATION_NOISE_DECAY>
+template <typename TI, bool T_DISTURBANCE, bool T_OBSERVATION_NOISE, bool T_ASYMMETRIC_ACTOR_CRITIC, bool T_ROTOR_DELAY, bool T_ACTION_HISTORY, bool T_ENABLE_CURRICULUM, bool T_RECALCULATE_REWARDS, bool T_EXPLORATION_NOISE_DECAY>
 struct AblationSpecTemplate: AblationSpecBase<TI> {
     static constexpr bool DISTURBANCE = T_DISTURBANCE;
     static constexpr bool OBSERVATION_NOISE = T_OBSERVATION_NOISE;
@@ -60,11 +60,9 @@ struct AblationSpecTemplate: AblationSpecBase<TI> {
     static constexpr bool ROTOR_DELAY = T_ROTOR_DELAY;
     static constexpr bool ACTION_HISTORY = T_ACTION_HISTORY;
     static constexpr bool ENABLE_CURRICULUM = T_ENABLE_CURRICULUM;
-    static constexpr bool USE_INITIAL_REWARD_FUNCTION = T_USE_INITIAL_REWARD_FUNCTION;
     static constexpr bool RECALCULATE_REWARDS = T_RECALCULATE_REWARDS;
     static constexpr bool EXPLORATION_NOISE_DECAY = T_EXPLORATION_NOISE_DECAY;
     static_assert(!ACTION_HISTORY || ROTOR_DELAY); // action history implies rotor delay
-    static_assert(!ENABLE_CURRICULUM || USE_INITIAL_REWARD_FUNCTION); // curriculum implies initial reward function
 };
 
 int main(int argc, char** argv){
@@ -83,20 +81,20 @@ int main(int argc, char** argv){
     }
     TI ablation_id = job_array_id / AblationSpecBase<TI>::NUM_RUNS;
     TI run_id = job_array_id % AblationSpecBase<TI>::NUM_RUNS;
-    // T_DISTURBANCE T_OBSERVATION_NOISE T_ASYMMETRIC_ACTOR_CRITIC T_ROTOR_DELAY T_ACTION_HISTORY T_ENABLE_CURRICULUM T_USE_INITIAL_REWARD_FUNCTION T_RECALCULATE_REWARDS T_EXPLORATION_NOISE_DECAY
+    // T_DISTURBANCE T_OBSERVATION_NOISE T_ASYMMETRIC_ACTOR_CRITIC T_ROTOR_DELAY T_ACTION_HISTORY T_ENABLE_CURRICULUM T_RECALCULATE_REWARDS T_EXPLORATION_NOISE_DECAY
 
-    using ABLATION_SPEC_00 = AblationSpecTemplate<TI, true,  true,  true,  true,  true,  true,  true,  true, true>;
-    using ABLATION_SPEC_01 = AblationSpecTemplate<TI, true,  true,  true,  true,  true, false,  true,  true, true>;
-    using ABLATION_SPEC_02 = AblationSpecTemplate<TI, true,  true,  true,  true,  true, false, false,  true, true>;
-    using ABLATION_SPEC_03 = AblationSpecTemplate<TI, true,  true,  true,  true, false,  true,  true,  true, true>;
-    using ABLATION_SPEC_04 = AblationSpecTemplate<TI, true,  true,  true, false, false,  true,  true,  true, true>;
-    using ABLATION_SPEC_05 = AblationSpecTemplate<TI, true,  true, false,  true,  true,  true,  true,  true, true>;
-    using ABLATION_SPEC_06 = AblationSpecTemplate<TI, true, false,  true,  true,  true,  true,  true,  true, true>;
-    using ABLATION_SPEC_07 = AblationSpecTemplate<TI,false,  true,  true,  true,  true,  true,  true,  true, true>;
-    using ABLATION_SPEC_08 = AblationSpecTemplate<TI, true,  true,  true,  true,  true,  true,  true, false, true>;
-    using ABLATION_SPEC_09 = AblationSpecTemplate<TI, true,  true, false,  true,  true, false,  true,  true, true>;
-    using ABLATION_SPEC_10 = AblationSpecTemplate<TI, true,  true, false,  true,  true, false, false,  true, true>;
-    using ABLATION_SPEC_11 = AblationSpecTemplate<TI, true,  true,  true,  true,  true,  true,  true,  true, false>;
+    using ABLATION_SPEC_00 = AblationSpecTemplate<TI, true,  true,  true,  true,  true,  true,  true, true>;
+    using ABLATION_SPEC_01 = AblationSpecTemplate<TI, true,  true,  true,  true,  true, false,  true, true>;
+    using ABLATION_SPEC_02 = AblationSpecTemplate<TI, true,  true,  true,  true,  true, false,  true, true>;
+    using ABLATION_SPEC_03 = AblationSpecTemplate<TI, true,  true,  true,  true, false,  true,  true, true>;
+    using ABLATION_SPEC_04 = AblationSpecTemplate<TI, true,  true,  true, false, false,  true,  true, true>;
+    using ABLATION_SPEC_05 = AblationSpecTemplate<TI, true,  true, false,  true,  true,  true,  true, true>;
+    using ABLATION_SPEC_06 = AblationSpecTemplate<TI, true, false,  true,  true,  true,  true,  true, true>;
+    using ABLATION_SPEC_07 = AblationSpecTemplate<TI,false,  true,  true,  true,  true,  true,  true, true>;
+    using ABLATION_SPEC_08 = AblationSpecTemplate<TI, true,  true,  true,  true,  true,  true, false, true>;
+    using ABLATION_SPEC_09 = AblationSpecTemplate<TI, true,  true, false,  true,  true, false,  true, true>;
+    using ABLATION_SPEC_10 = AblationSpecTemplate<TI, true,  true, false,  true,  true, false,  true, true>;
+    using ABLATION_SPEC_11 = AblationSpecTemplate<TI, true,  true,  true,  true,  true,  true,  true, false>;
 
     switch(ablation_id){
         case 0:
