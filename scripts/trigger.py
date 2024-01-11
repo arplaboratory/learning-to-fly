0a4e6895024421a50f6b71a3a6364d1c09ecd148
Jonas Eschmann <jonas.eschmann@gmail.com>
2024-01-11 10:05:37 +0400

    adding take-off and handover mode to the trigger script

--- scripts/trigger.py
+++ scripts/trigger.py
@@ -108,13 +108,44 @@ def mode_trajectory_tracking(cf, args):
         else:
             send_learned_policy_packet(cf)
 
+def mode_takeoff_and_switch(cf, args):
+    set_param(cf, "rlt.trigger", 0) # setting the trigger mode to the custom command  (cf. https://github.com/arplaboratory/learning_to_fly_controller/blob/0a7680de591d85813f1cd27834b240aeac962fdd/rl_tools_controller.c#L80)
+    set_param(cf, "rlt.wn", 1)
+    set_param(cf, "rlt.target_z", 0)
+
+    input("Press enter to start hovering")
+    prev = time.time()
+    acc = 0
+    cnt = 0
+    start_time = time.time()
+    while True:
+        i = input("Hold enter to fly")
+        if i == "q":
+            break
+        current = time.time()
+        acc += current - prev
+        cnt += 1
+        if cnt % 100 == 0:
+            print(f"Average rate: {1/(acc / cnt):.3f}Hz")
+            acc = 0
+            cnt = 0
+        now = time.time()
+        if current - prev > 0.1:
+            start_time = now
+        prev = current
+
+        if now - start_time < args.transition_timeout:
+            send_hover_packet(cf, args.height)
+        else:
+            send_learned_policy_packet(cf)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     default_uri = 'radio://0/80/2M/E7E7E7E7E7'
     parser.add_argument('--uri', default=default_uri)
     parser.add_argument('--height', default=0.2, type=float)
-    parser.add_argument('--mode', default='hover_learned', choices=['hover_learned', 'hover_original', 'trajectory_tracking'])
+    parser.add_argument('--mode', default='hover_learned', choices=['hover_learned', 'hover_original', 'takeoff_and_switch', 'trajectory_tracking'])
     parser.add_argument('--trajectory-scale', default=1, type=float, help="Scale of the trajectory")
     parser.add_argument('--trajectory-interval', default=5.5, type=float, help="Interval of the trajectory")
     parser.add_argument('--transition-timeout', default=3, type=float, help="Time after takeoff with the original controller after which the learned controller is used for trajectory tracking")
@@ -128,6 +159,8 @@ if __name__ == '__main__':
             mode_hover_learned(scf.cf, args)
         elif args.mode == "hover_original":
             mode_hover_original(scf.cf, args)
+        elif args.mode == "takeoff_and_switch":
+            mode_takeoff_and_switch(scf.cf, args)
         elif args.mode == "trajectory_tracking":
             mode_trajectory_tracking(scf.cf, args)
         else:
